3d20d0ae620b8cef1cf4e3498a7efb6efa7a2180
matimacayal <keny92@gmail.com>
2024-07-29 19:40:33 -0400

    feat: Enemy comes back to initial position when not chasing player

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,13 +5,21 @@ var player_chase = false
 var player = null
 var health = 20
 var dying = false
+var initial_position = null
+var returning = false
+
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var timer = $detection_area/Timer
+
+func _ready():
+	initial_position = position
 
 func _physics_process(delta):
 	if dying:
 		return
 	
-	if player_chase:
+	if player_chase && !returning:
+		print("Chasing player")
 		var chase_vector = (player.position - position).normalized()
 		velocity = chase_vector # * speed
 		# position += chase_vector * speed * delta
@@ -28,15 +36,39 @@ func _physics_process(delta):
 		velocity = lerp(velocity, Vector2.ZERO, 0.22)
 		animated_sprite.play("idle")
 	
+	if !player_chase && returning:
+		var return_distance = initial_position - position
+		var chase_vector = return_distance.normalized()
+		
+		
+		var distance = return_distance.length()
+		#print("return_distance " + str(distance))
+		if distance < 1:
+			print("Enemy returned to initial position")
+			position = initial_position
+			chase_vector = Vector2.ZERO
+			returning = false
+
+		velocity = chase_vector  * speed
+		move_and_slide()
+		return
+	
 	move_and_collide(velocity)
 
 func _on_detection_area_body_entered(body):
 	player = body
 	player_chase = true
+	returning = false
+	timer.stop()
 
 func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
+	
+	timer.start()
+	
+func _on_timer_timeout():
+	returning = true
 
 func take_hit(damage):
 	health -= damage
@@ -50,3 +82,5 @@ func take_hit(damage):
 func _on_animated_sprite_2d_animation_finished():
 	if health <= 0:
 		queue_free()
+
+
